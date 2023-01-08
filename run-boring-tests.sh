f8df800d188b412c113b4978916c3841776854e6
Nigel Tao <nigeltao@golang.org>
2023-01-08 11:55:26 +1100

    Have run-boring-tests.sh cover more images

--- run-boring-tests.sh
+++ run-boring-tests.sh
@@ -1,23 +1,31 @@
 #!/bin/bash -eu
 
 # This is just a very basic consistency check, comparing the cjpeg and djpeg
-# programs' output on a single golden test case each.
+# programs' output on a small suite of golden test cases.
 
 if [ ! -f ./cjpeg ] || [ ! -f ./djpeg ]; then
     echo "./cjpeg or ./djpeg not found; run a build-*.sh script first"
     exit 1
 fi
 
-CHASH=$(./cjpeg testimages/testorig.ppm | sha256sum | cut -d ' ' -f1)
-if [ $CHASH != "491679b8057739b3c8e5bacd1e918efb1691d271cbbd69820ff8d480dcb90963" ]; then
-    echo "./cjpeg produced unexpected output"
-    exit 1
-fi
-
-DHASH=$(./djpeg testimages/testorig.jpg | sha256sum | cut -d ' ' -f1)
-if [ $DHASH != "b64a47d529e289e5570648db9f0385d9b240448724cdde7716fa0139b121d491" ]; then
-    echo "./djpeg produced unexpected output"
-    exit 1
-fi
+while read LINE; do
+    A=($LINE)
+    if [[ -z $LINE || ${A[0]} == "#" ]]; then
+        continue
+    elif [[ ${A[1]} == *.ppm ]]; then
+        HASH=$(./cjpeg ${A[1]} | sha256sum | cut -d ' ' -f1)
+    elif [[ ${A[1]} == *.jpeg || ${A[1]} == *.jpg ]]; then
+        HASH=$(./djpeg ${A[1]} | sha256sum | cut -d ' ' -f1)
+    else
+        echo "UNEXPECTED $LINE"
+        exit 1
+    fi
+    if [[ $HASH != ${A[0]} ]]; then
+        echo "HAVE $HASH"
+        echo "WANT ${A[0]}"
+        echo "FAIL ${A[1]}"
+        exit 1
+    fi
+done <testimages/boring-tests-hashes.txt
 
 echo PASS
